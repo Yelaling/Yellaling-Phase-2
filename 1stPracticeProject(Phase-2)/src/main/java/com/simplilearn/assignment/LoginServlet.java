127ed8511ff79b029372f1c0e3e45b39bb207f88
unknown <yellaling18.jsp.btr@gmail.com>
2022-02-22 20:28:34 +0530

    first commit

---
+++ 1stPracticeProject(Phase-2)/src/main/java/com/simplilearn/assignment/LoginServlet.java
@@ -0,0 +1,45 @@
+package com.simplilearn.assignment;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		String name= request.getParameter("userid");
+		String pass= request.getParameter("password");
+		PrintWriter out=response.getWriter();
+		if(pass.equals("admin123"))
+		{
+			out.print("<form action='dashboard'>");
+			out.print("<input type='hidden' name='user' value='"+name+"'>");
+			out.print("<input type='hidden' name='pass' value='"+pass+"'>");
+			out.print("<input type='submit' value='login successfuly'>");
+			out.print("</form>");
+		}
+		
+		else
+		{
+			out.print("invalid user");
+		}
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
